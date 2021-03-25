ee090324b21f8ea4532234bb85539ecf478f4a58
Tasnim Alam <tasnimalamcse@gmail.com>
2021-03-25 12:23:01 +0600

    Update git submodules

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-polyfill'), 'version' => '6177e16cf242c0cd2f75fa67bc41a714');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-polyfill'), 'version' => '4780362f686873a3eb1623498b80824c');
